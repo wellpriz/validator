ea91a69c5bf5fa1d014a5d8c092a38e0e12404fc
Wellington Jonathan <wellpriz@gmail.com>
2016-08-21 02:17:55 -0300

    iniciando models

--- src/Model/Entity/Aluno.php
+++ src/Model/Entity/Aluno.php
@@ -3,8 +3,18 @@ namespace App\model\Entity;
 use Cake\ORM\Entity;
 
 class Aluno extends Entity {
-
-
+    protected  $_accessible =[
+    			'nome' => true,
+                'cpf' => true,
+                'rg' => true,
+                'datanasc' => true,
+                'curso' => true,
+                'matricula' => true,
+                'semestre' => true,
+                'unidade' => true,
+                'url' => true,
+                ];
+              
 }
 
 ?>
--- src/Model/Table/AlunosTable.php
+++ src/Model/Table/AlunosTable.php
@@ -4,6 +4,14 @@ use Cake\Database\Schema\Table;
 
 class AlunosTable extends Table{
 
+    /**
+     * @param array $config
+     */
+    public function initialize(array $config ){
+
+        $this->addBehaviuor('Timestamp');
+        $this->displayField('nome');
+    }
 
 }
 
